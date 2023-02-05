621d46482b1f257fa9d2d7c762f46dd5ec8e7768
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-02-05 14:46:05 +0100

    add ExpensesList component, add showing and hiding ExpenseForm

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,17 +24,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-      {filteredExpenses.length > 0 && (
-      filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))
-      )}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  // HANDLING FORM SUBMITTING AND FORM DATA PROCESSING
+
   // separate State approach
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
@@ -78,43 +80,70 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+  // HANDLING SHOWING/HIDING FORM BY BUTTON CLICKING
+
+  const [showNewExpenseForm, setShowNewExpenseForm] = useState(false);
+
+  const showExpenseFormHandler = () => {
+    setShowNewExpenseForm(true);
+  };
+
+  const hideExpenseFormHandler = () => {
+    setShowNewExpenseForm(false);
+  };
+
+  if (showNewExpenseForm == true) {
+    return (
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2023-12-31"
+              value={enteredDate}
+              onChange={dateChangeHandler}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+        <div>
+          <button type="button" onClick={hideExpenseFormHandler}>
+            Cancel
+          </button>
+          <button type="submit">Add Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2023-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
+      </form>
+    );
+  } else {
+    return (
+      <div className="new-expense__control">
+        <div>
+          <button type="button" onClick={showExpenseFormHandler}>
+            New Expense
+          </button>
         </div>
       </div>
-      <div>
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
-  );
+    );
+  }
 };
 
 export default ExpenseForm;
